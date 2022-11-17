ba3e1d0ebe1e5eaf6eb2185f961a75d10e9c5a14
oluwadamilareadeniye@yahoo.com <oluwadamilareadeniye@yahoo.com>
2022-11-18 00:14:41 +0800

    create the Booking model

--- src/hotel/models.py
+++ src/hotel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -17,3 +18,13 @@ class Room(models.Model):
 
     def __str__(self):
         return f'{self.number}. {self.category} with {self.beds} for {self.capacity} people'
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user} has booked {self.room} from {self.check_in} to {self.check_out}"
+
