1ca4aef9448253200b5ab8d83bcab7ba259cc52d
oluwadamilareadeniye@yahoo.com <oluwadamilareadeniye@yahoo.com>
2022-11-17 20:50:16 +0800

    Connect to mysql server and set up the Room model

--- src/hotel/models.py
+++ src/hotel/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+ROOM_CATEGORIES = (
+    ('Basic', 'Basic'),
+    ('Diamond', 'Diamond'),
+    ('Gold', 'Gold'),
+    ('Silver', 'Silver')
+)
+
+class Room(models.Model):
+    number = models.IntegerField()
+    category = models.CharField(max_length=7, choices=ROOM_CATEGORIES)
+    beds = models.IntegerField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.number}. {self.category} with {self.beds} for {self.capacity} people'
